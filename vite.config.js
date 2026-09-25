eab0b4bd91a495a446ce9964316263690b3aa843
Drew Steindl <Drewsteindl@icloud.com>
2026-09-25 12:44:19 -0400

    Add CurrentDrew board and GitHub sync setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    {
+      name: 'currentdrew-directory-index',
+      configureServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          if (req.url === '/CurrentDrew' || req.url === '/CurrentDrew/') {
+            req.url = '/CurrentDrew/index.html'
+          }
+          next()
+        })
+      },
+    },
+  ],
 })
